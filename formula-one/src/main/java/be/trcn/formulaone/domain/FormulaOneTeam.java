11f136080cdadb71e52f6c8c1dd8e20dd913cb2e
HursitTarcan <HursitTarcan@gmail.com>
2022-05-28 14:05:32 +0200

    Entity Class Done

--- formula-one/src/main/java/be/trcn/formulaone/domain/FormulaOneTeam.java
+++ formula-one/src/main/java/be/trcn/formulaone/domain/FormulaOneTeam.java
@@ -1,4 +1,45 @@
 package be.trcn.formulaone.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "formulaoneteams")
 public class FormulaOneTeam {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int members;
+    private String topDriver;
+
+    public FormulaOneTeam() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMembers() {
+        return members;
+    }
+
+    public void setMembers(int members) {
+        this.members = members;
+    }
+
+    public String getTopDriver() {
+        return topDriver;
+    }
+
+    public void setTopDriver(String topDriver) {
+        this.topDriver = topDriver;
+    }
 }
